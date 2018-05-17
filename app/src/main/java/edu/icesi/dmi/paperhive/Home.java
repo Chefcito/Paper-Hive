d4b86431239c1a19d0647e252d6aa796ad514b3a
ParedesAndres <35270955+ParedesAndres@users.noreply.github.com>
2018-05-17 04:42:54 -0500

    Create variable

--- app/src/main/java/edu/icesi/dmi/paperhive/Home.java
+++ app/src/main/java/edu/icesi/dmi/paperhive/Home.java
@@ -2,9 +2,12 @@ package edu.icesi.dmi.paperhive;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
+    Button prueba;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
